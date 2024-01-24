bdfef46fae9e8fdb9277f6462a37c875b8ee5001
Sergey Kabanets <skabanets97@gmail.com>
2024-01-24 20:52:51 +0200

    add loader and error notification

--- src/components/Phonebook/Phonebook.jsx
+++ src/components/Phonebook/Phonebook.jsx
@@ -3,12 +3,26 @@ import { Contacts } from 'components/Contacts/Contacts';
 import { PhonebookWrapper, Subtitle, Title } from './Phonebook.styled';
 import { Filter } from 'components/Filter/Filter';
 import { useDispatch, useSelector } from 'react-redux';
-import { selectContacts } from '../../redux/contacts/slice';
+import {
+  selectContacts,
+  selectError,
+  selectIsLoading,
+} from '../../redux/contacts/slice';
 import { useEffect } from 'react';
 import { fetchContacts } from '../../redux/contacts/operation';
+import { Loader } from 'components/Loader/Loader';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 export const Phonebook = () => {
   const contacts = useSelector(selectContacts);
+  const isLoading = useSelector(selectIsLoading);
+  const error = useSelector(selectError);
+
+  if (error) {
+    toast.error('Oops... Something wrong! Reload page or try again later.');
+  }
+
   const dispatch = useDispatch();
 
   useEffect(() => {
@@ -17,11 +31,13 @@ export const Phonebook = () => {
 
   return (
     <PhonebookWrapper>
+      {isLoading && <Loader />}
       <Title>Phonebook</Title>
       <ContactForm />
       <Subtitle>Contacts ({contacts.length})</Subtitle>
       <Filter />
       {contacts.length ? <Contacts /> : <p>No contacts</p>}
+      <ToastContainer autoClose={3000} />
     </PhonebookWrapper>
   );
 };
--- src/redux/contacts/slice.js
+++ src/redux/contacts/slice.js
@@ -1,8 +1,10 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, isAnyOf } from '@reduxjs/toolkit';
 import { addContact, deleteContact, fetchContacts } from './operation';
 
 const initialState = {
   contacts: [],
+  isLoading: false,
+  error: null,
 };
 
 const slice = createSlice({
@@ -19,12 +21,46 @@ const slice = createSlice({
       .addCase(deleteContact.fulfilled, (state, { payload }) => {
         const index = state.contacts.findIndex(item => item.id === payload.id);
         state.contacts.splice(index, 1);
-      });
+      })
+      .addMatcher(
+        isAnyOf(
+          fetchContacts.pending,
+          addContact.pending,
+          deleteContact.pending
+        ),
+        state => {
+          state.isLoading = true;
+          state.error = null;
+        }
+      )
+      .addMatcher(
+        isAnyOf(
+          fetchContacts.fulfilled,
+          addContact.fulfilled,
+          deleteContact.fulfilled
+        ),
+        state => {
+          state.isLoading = false;
+        }
+      )
+      .addMatcher(
+        isAnyOf(
+          fetchContacts.rejected,
+          addContact.rejected,
+          deleteContact.rejected
+        ),
+        (state, { payload }) => {
+          state.isLoading = false;
+          state.error = payload;
+        }
+      );
   },
   selectors: {
     selectContacts: state => state.contacts,
+    selectIsLoading: state => state.isLoading,
+    selectError: state => state.error,
   },
 });
 
 export const contactsReducer = slice.reducer;
-export const { selectContacts } = slice.selectors;
+export const { selectContacts, selectIsLoading, selectError } = slice.selectors;
